e6736dd2d80d0ef069d17b983c42d77bd30c43ee
Aruna Jayarajan <runa.jayarajan@gmail.com>
2020-08-02 20:29:30 +0530

    Assigment_2_R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Getting and setting values of the vecto and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+ p <- NULL
+ set <- function(y){
+   x <<- y
+   p <<- NULL
+    
+ }
+  get <-function()x
+  setInv <-function(inverse)p <-- inverse
+  getInv <- function()p
+  list(set = set, get= get , setInv= setInv, getInv=getInv)
 }
 
 
-## Write a short comment describing this function
-
+## Computing the matrix and returning the inverse
 cacheSolve <- function(x, ...) {
+  p<- x$getInv()
+  if(!is.null(p)){
+    message("Getting Cached Data")
+    return(p)
+  }
+  mat <- x$get()
+  p <-solve(mat,...)
+  x$setInv(p)
+  p
         ## Return a matrix that is the inverse of 'x'
 }
